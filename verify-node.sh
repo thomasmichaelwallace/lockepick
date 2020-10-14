679cd6448cdba1e7b80e61ddbd13c7cf900b074b
thomasmichaelwallace <tom.wallace@devicepilot.com>
2020-10-14 10:22:24 +0100

    feat: transpile go library to node

---
+++ verify-node.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd ./lib/ff6-release4-js
+
+rm -rf ./verify
+mkdir ./verify
+cp ../../tmp/data ./verify
+
+node ./ff6-release4.js tojson -i ./verify/data -o ./verify/verify.json
+node ./ff6-release4.js fromjson -i ./verify/verify.json -o ./verify/verify.sav
+
+diff ./verify/data ./verify/verify.sav
