3fbf7e3b8e9decdae3dc619c2b187aa2b1cc899f
thomasmichaelwallace <tom.wallace@devicepilot.com>
2020-10-14 14:51:43 +0100

    feat: verify module build correctness

--- verify-node.sh
+++ verify-node.sh
@@ -6,7 +6,8 @@ rm -rf ./verify
 mkdir ./verify
 cp ../../tmp/data ./verify
 
-node ./ff6-release4.js tojson -i ./verify/data -o ./verify/verify.json
-node ./ff6-release4.js fromjson -i ./verify/verify.json -o ./verify/verify.sav
+# node ./ff6-release4.js tojson -i ./verify/data -o ./verify/verify.json
+# node ./ff6-release4.js fromjson -i ./verify/verify.json -o ./verify/verify.sav
+node test.js
 
 diff ./verify/data ./verify/verify.sav
