8278936b24f9207948c6b460fc31ab8ed66779a3
thomasmichaelwallace <tom.wallace@devicepilot.com>
2020-10-14 15:05:09 +0100

    fix: restore verify-go checks

---
+++ lib/ff6-release4-bin/main.go
@@ -0,0 +1,75 @@
+// Copyright 2014
+// Use of this source code is governed by a 2-clause
+// BSD-style license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"flag"
+	"github.com/robmerrell/comandante"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+var (
+	savfile  = "filesRockDat0.sav"
+	jsonfile = "filesRockDat0.json"
+)
+
+func tojson() error {
+	data, err := ioutil.ReadFile(savfile)
+	if err != nil {
+		return err
+	}
+	
+	d, err := ToJsonBytes(data)
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(jsonfile, d, 0644)
+}
+
+func fromjson() error {
+	data, err := ioutil.ReadFile(jsonfile)
+	if err != nil {
+		return err
+	}
+
+	s, err := FromJsonBytes(data)
+	if err != nil {
+		return err
+	}
+	_, err = os.Stat(savfile)
+	if err == nil {
+		_, err = os.Stat(savfile + ".bak")
+		if err != nil {
+			os.Rename(savfile, savfile+".bak")
+		}
+	}
+	return ioutil.WriteFile(savfile, s, 0644)
+}
+
+func main() {
+	bin := comandante.New("ff6", "Manage Final Fantasy VI mobile save files")
+	bin.IncludeHelp()
+	tj := comandante.NewCommand("tojson", "Convert .sav save file to .json", tojson)
+	tj.FlagInit = func(fs *flag.FlagSet) {
+		fs.StringVar(&savfile, "i", savfile, "Input .sav file")
+		fs.StringVar(&jsonfile, "o", jsonfile, "Output .json file")
+	}
+	bin.RegisterCommand(tj)
+	fj := comandante.NewCommand("fromjson", "Convert .json save file to .sav", fromjson)
+	fj.FlagInit = func(fs *flag.FlagSet) {
+		fs.StringVar(&jsonfile, "i", jsonfile, "Input .json file")
+		fs.StringVar(&savfile, "o", savfile, "Output .sav file")
+	}
+	bin.RegisterCommand(fj)
+
+	flag.Parse()
+
+	if err := bin.Run(); err != nil {
+		log.Fatalln(err)
+	}
+}
--- verify-go.sh
+++ verify-go.sh
@@ -2,18 +2,23 @@
 
 set -e
 
-cd ./lib/ff6-release4
-rm -f ./ff6-release4
+cd ./lib/ff6-release4-bin
+rm -rf ./ff6-release4
+
+cp -rf ../ff6-release4 ./
+cp -f ./main.go ./ff6-release4
+cd ./ff6-release4
 
 go build
 
 rm -rf ./verify
 mkdir ./verify
-cp ../../tmp/data ./verify
+cp ../../../tmp/data ./verify
 
 echo to data...
 ./ff6-release4 tojson -i ./verify/data -o ./verify/verify.json
 echo from data...
 ./ff6-release4 fromjson -i ./verify/verify.json -o ./verify/verify.sav
 
+echo diff:
 diff ./verify/data ./verify/verify.sav
