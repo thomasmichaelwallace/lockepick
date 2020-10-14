95e4e84d6586c529dcb8ff040a4c95df59ea4c56
thomasmichaelwallace <tom.wallace@devicepilot.com>
2020-10-14 21:18:45 +0100

    chore: organise scripts

--- scripts/compile-js.sh
+++ scripts/compile-js.sh
@@ -5,4 +5,4 @@ cp -r ./lib/ff6-release4 $GOPATH/src/github.com/quarnster
 
 cd ./lib/ff6-release4-js
 
-$GOPATH/bin/gopherjs build github.com/quarnster/ff6-release4
+GOPHERJS_GOROOT=$HOME/sdk/go1.12.16 $GOPATH/bin/gopherjs build github.com/quarnster/ff6-release4
